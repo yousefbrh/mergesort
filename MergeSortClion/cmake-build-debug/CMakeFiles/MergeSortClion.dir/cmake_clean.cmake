5906f20ac993fe4dbaf480692a575a96de3f7918
Yousef Borhani <yousefborhanii@gmail.com>
2020-02-06 17:59:29 +0330

    Starting the Project

---
+++ MergeSortClion/cmake-build-debug/CMakeFiles/MergeSortClion.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MergeSortClion.dir/main.cpp.obj"
+  "MergeSortClion.exe"
+  "MergeSortClion.exe.manifest"
+  "MergeSortClion.pdb"
+  "libMergeSortClion.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MergeSortClion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
